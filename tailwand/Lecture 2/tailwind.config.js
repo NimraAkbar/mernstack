b32292a4843c24904470b4d6f5cfd3f111982a77
Nimra Mughal <mughalrimsha562@gmail.com>
2024-11-26 22:19:43 +0500

    new changes

---
+++ tailwand/Lecture 2/tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    colors:{
+        blue: {
+            100:'#1fb6ff',
+            200:'#1fb6ff',
+            390:'#1fb6ff',
+            600:'#1fb6ff',
+        },
+        'purple': '#7e5bef',
+        'pink': '#ff49db',
+        'orange': '#ff7849',
+        'green': '#13ce66',
+        'yellow': '#ffc82c',
+        'gray-dark': '#273444',
+        'gray': '#8492a6',
+        'gray-light': '#d3dce6',
+      },
+    extend: {
+        screens: {
+            'nimo': {'min': '640px', 'max': '767px'},
+        },
+    },
+    },
+  plugins: [],
+}
